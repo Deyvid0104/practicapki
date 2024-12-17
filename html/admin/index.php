3c9b12f82fe607ce4a4bb817cf4fd9d85314d550
Deyvid0104 <driotol124@g.educaand.es>
2024-12-17 12:22:31 +0100

    Modificacion del Header y Footer en los ficheros index.html, index.php y cert_info.php

--- html/admin/index.php
+++ html/admin/index.php
@@ -7,31 +7,34 @@
     <link rel="stylesheet" href="../asserts/styles.css">
 </head>
 <body>
-    <h1>Registros de Acceso</h1>
+    <header>
+        <h1>Registros de Acceso</h1>
+    </header>
 
     <?php
-    $log_file = '/var/log/user_access.log';
+        $log_file = '/var/log/user_access.log';
 
-    // Verificar si el archivo de log existe
-    if (!file_exists($log_file)) {
-        echo "<p>No hay registros disponibles.</p>";
-        exit;
-    }
+        // Verificar si el archivo de log existe
+        if (!file_exists($log_file)) {
+            echo "<p>No hay registros disponibles.</p>";
+            exit;
+        }
 
-    // Leer las entradas del archivo de log
-    $log_entries = file($log_file, FILE_IGNORE_NEW_LINES);
+        // Leer las entradas del archivo de log
+        $log_entries = file($log_file, FILE_IGNORE_NEW_LINES);
 
-    // Renderizar la tabla
-    echo "<table border='1'>";
-    echo "<thead><tr><th>Fecha</th><th>Hora</th><th>Usuario</th><th>Correo</th><th>IP</th></tr></thead>";
-    echo "<tbody>";
-    foreach ($log_entries as $entry) {
-        list($fecha, $hora, $user, $email, $ip) = explode(", ", $entry);
-        echo "<tr><td>$fecha</td><td>$hora</td><td>$user</td><td>$email</td><td>$ip</td></tr>";
-    }
-    echo "</tbody>";
-    echo "</table>";
+        // Renderizar la tabla
+        echo "<table border='1'>";
+        echo "<thead><tr><th>Fecha</th><th>Hora</th><th>Usuario</th><th>Correo</th><th>IP</th></tr></thead>";
+        echo "<tbody>";
+        foreach ($log_entries as $entry) {
+            list($fecha, $hora, $user, $email, $ip) = explode(", ", $entry);
+            echo "<tr><td>$fecha</td><td>$hora</td><td>$user</td><td>$email</td><td>$ip</td></tr>";
+        }
+        echo "</tbody>";
+        echo "</table>";
     ?>
+
     <footer>
         <p>&copy; Deyvid Rios - 2024. Sistema PKI.</p>
     </footer>
